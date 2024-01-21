5fd7f23016a3cdf2815ebe82b2ce8bf011a15ab9
Kenichiro Tsuji <ken1029.study@gmail.com>
2024-01-21 14:35:07 +0900

    2章完了

--- chat/main.go
+++ chat/main.go
@@ -1,32 +1,69 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 	"sync"
 	"text/template"
+
+	"github.com/joho/godotenv"
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/google"
+	"github.com/stretchr/objx"
 )
 
 type templateHandler struct {
 	once     sync.Once
 	filename string
-	temp1    *template.Template
+	templ    *template.Template
 }
 
 func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
-		t.temp1 =
+		t.templ =
 			template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.temp1.Execute(w, nil)
+
+	data := map[string]interface{}{
+		"Host": r.Host,
+	}
+	if authCookie, err := r.Cookie("auth"); err == nil {
+		data["UserData"] = objx.MustFromBase64(authCookie.Value)
+	}
+
+	t.templ.Execute(w, data)
 }
 
 func main() {
+	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
+	flag.Parse() // フラグを解釈
+
+	// Gomniauthのセットアップ
+	godotenv.Load(".env")
+	gomniauth.SetSecurityKey("2Gken1029")
+	gomniauth.WithProviders(
+		google.New(
+			os.Getenv("google_client_id"), 
+			os.Getenv("secret_key"), 
+			"http://localhost:8080/auth/callback/google",
+		),
+	)
+
+	r := newRoom()
+	// r.tracer = trace.New(os.Stdout)
 	// ルート
-	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
+	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
+	http.Handle("/room", r)
+	// チャットルームを開始します
+	go r.run()
+	log.Println("Webサーバを開始。ポート:", *addr)
 	// Webサーバを開始します
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
 }
